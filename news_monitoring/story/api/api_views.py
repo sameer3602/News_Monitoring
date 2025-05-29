0aacf1236435fcaf638b53c0ce696d6291d0bd49
sameer3602 <sameerkhan.1234asd@gmail.com>
2025-05-29 10:04:13 +0530

    optimized story views

--- news_monitoring/story/api/api_views.py
+++ news_monitoring/story/api/api_views.py
@@ -17,21 +17,4 @@ class StoryViewSet(viewsets.ModelViewSet):
         # Assuming user.company is a Company instance or ID
         return Story.objects.filter(tagged_companies__in=[user.company])
     
-    # Optional: Ensure create/update/delete actions validate company ownership 
-    def perform_create(self, serializer):
-        # You could add logic to assign user/company here if needed
-        serializer.save()
-
-    def perform_update(self, serializer):
-        story = self.get_object()
-        user = self.request.user
-        if not user.is_staff and user.company not in story.tagged_companies.all():
-            raise PermissionDenied("You don't have permission to edit this story.")
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        user = self.request.user
-        if not user.is_staff and user.company not in instance.tagged_companies.all():
-            raise PermissionDenied("You don't have permission to delete this story.")
-        instance.delete()
 
--- news_monitoring/story/api/serializers.py
+++ news_monitoring/story/api/serializers.py
@@ -20,12 +20,29 @@ class StorySerializer(serializers.ModelSerializer):
         queryset=Company.objects.all()
     )
     tagged_companies = CompanySerializer(many=True, read_only=True)
+    
+    # for updating the story
     def update(self, instance, validated_data):
         company_ids = validated_data.pop('tagged_companies_ids', None)
         instance = super().update(instance, validated_data)
         if company_ids is not None:
             instance.tagged_companies.set(company_ids)
         return instance
+
+    # for creating the story
+    def create(self, validated_data):
+        company_ids = validated_data.pop('tagged_companies_ids', [])
+        story = Story.objects.create(**validated_data)
+        story.tagged_companies.set(company_ids)
+        return story
+
+    # for deleting the story
+    def delete(self, instance):
+        user = self.request.user
+        if not user.is_staff and user.company not in instance.tagged_companies.all():
+            raise PermissionDenied("You don't have permission to delete this story.")
+        instance.delete()
+
     class Meta:
         model = Story
         fields = '__all__'
